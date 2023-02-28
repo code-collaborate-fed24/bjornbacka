e55b6e41f7842da4e7c24e3dd5cfac169cd3cb41
amin <amin.haghi@hyperisland.se>
2023-03-01 00:15:09 +0100

    header - not responsive-waiting for ux dsnrs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
