b6a3eacdbf8804566fb41bb999099f14bd31a1e8
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-03-08 00:27:47 +0100

    two events added

--- src/components/Events/index.js
+++ src/components/Events/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import "./events.css"
+import EventCard from './eventCard'
 const Events = () => {
     return (
-        <div>Events</div>
+        <section className='events-row'>
+            <EventCard className="event-card" eventImg="./Assets/Events/Garden.png" eventTitle="Utomhusaktiviteter" />
+            <EventCard className="event-card" eventImg="./Assets/Events/Cake.png" eventTitle="Vår matfilosofi" />
+        </section >
     )
 }
 
