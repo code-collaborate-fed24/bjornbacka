d5bf05bddd0623948dd78ea8f4271d2b9487c21f
amin <amin.haghi@hyperisland.se>
2023-03-07 16:05:30 +0100

    add kurser content-component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import Kurser from "../../components/Kurser";
 import Partners from "../../components/Partners";
-
+import "./home.css";
 function index() {
-  return (
-    <>
-      <Header />
-      <Partners />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<div className="homepage-container">
+				<Header />
+				<Partners />
+				<Kurser />
+				<Footer />
+			</div>
+		</>
+	);
 }
 
 export default index;
