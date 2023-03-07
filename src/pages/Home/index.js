93cef6726097e8d4a77aac342309112778a0ea79
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-03-07 16:03:04 +0100

    partner logos added

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import Partners from "../../components/Partners";
-
+import PartnerLogos from "../../components/partnersLogos";
 function index() {
   return (
     <>
       <Header />
       <Partners />
+      <PartnerLogos />
       <Footer />
     </>
   );
