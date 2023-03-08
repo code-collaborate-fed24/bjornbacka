bfa0ed007cc6e6e6e62182cb0c5e9d65b8d23608
amin <amin.haghi@hyperisland.se>
2023-03-08 15:25:21 +0100

    header-responsive

---
+++ src/components/Nav/Navbar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Burger from "./Burger";
+import "./navbar.css";
+
+const Nav = styled.nav`
+	width: 100%;
+	height: 80px;
+	border-bottom: 2px solid #f1f1f1;
+	padding: 1rem 1.2rem;
+	display: flex;
+	align-items: center;
+	background-color: black;
+	justify-content: space-between;
+	.logo {
+		padding: 15px 0;
+	}
+`;
+
+const Navbar = () => {
+	return (
+		<Nav>
+			<Link to="/">
+				<div className="icon">
+					<img src="./images/logo.svg" alt="logo" />
+					<img src="./images/logotxt.svg" alt="logotext" />
+				</div>
+			</Link>
+			<div className="searchbar">
+				<img
+					className="searchbar__img"
+					src="./images/iconsearch.svg"
+					alt="search"
+				/>
+				<input type="text" placeholder="Search" />
+			</div>
+			<Burger />
+		</Nav>
+	);
+};
+
+export default Navbar;
---
+++ src/components/Nav/RightNav.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Ul = styled.ul`
+	list-style: none;
+	display: flex;
+	flex-flow: row nowrap;
+	li {
+		padding: 18px 10px;
+		color: #ffffff;
+	}
+	li a {
+		color: #ffffff;
+	}
+	@media (max-width: 992px) {
+		flex-flow: column nowrap;
+		background-color: #0d2538;
+		position: fixed;
+		transform: ${({ open }) => (open ? "translateX(0)" : "translateX(100%)")};
+		top: 0;
+		right: 0;
+		height: 100vh;
+		width: 300px;
+		padding-top: 3.5rem;
+		transition: transform 0.3s ease-in-out;
+		li {
+			color: #fff;
+		}
+	}
+`;
+
+const RightNav = ({ open }) => {
+	return (
+		<Ul open={open}>
+			<li>
+				<Link to={"/"}>FORETAG</Link>
+			</li>
+			<li>
+				<Link to={"/"}>PRIVAT</Link>
+			</li>
+			<li>
+				<Link to={"/"}>KURSER OCH RETREATS</Link>
+			</li>
+			<li>
+				<Link to={"/booking"}>BOKA</Link>
+			</li>
+		</Ul>
+	);
+};
+
+export default RightNav;
