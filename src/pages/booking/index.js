c54be5f049d0de1bcbc524bd556b1fcf861c962b
Alejandra Rojas <alejandra.rojas@hyperisland.se>
2023-03-09 14:54:26 +0100

    booking component without styling and UI card component

--- src/pages/booking/index.js
+++ src/pages/booking/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import BookingComponent from "../../components/BookingComponent";
 import Partners from "../../components/Partners";
 
 function index() {
   return (
     <>
       <Header />
-      <h1>booking page</h1>
+      <BookingComponent />
     </>
   );
 }
