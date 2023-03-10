577c814939d6b0538f895d4d196dcdb8fdc00a64
amin <amin.haghi@hyperisland.se>
2023-03-10 16:57:58 +0100

    work on rersponsive part of card component

--- src/components/CardComponents/carditems.js
+++ src/components/CardComponents/carditems.js
@@ -4,21 +4,21 @@ import "./carditems.css";
 // import Pic3 from "../../Assets/CardComponentImages/pic3.JPG";
 
 const CardItem = (props) => {
-  return (
-    <div className="item-container">
-      <img className="images" src={props.image} />
-      <div className="text-wrapper">
-        <p className="visit-title">{props.title}</p>
-        <div className="description-wrapper">
-          <p className="description">{props.description}</p>
-        </div>
-        <div className="btns-wrapper">
-          <div className="left-btn">Read More</div>
-          <div className="right-btn">Book now</div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="item-container">
+			<img height={200} className="images" src={props.image} />
+			<div className="text-wrapper">
+				<p className="visit-title">{props.title}</p>
+				<div className="description-wrapper">
+					<p className="description">{props.description}</p>
+				</div>
+				<div className="btns-wrapper">
+					<div className="left-btn">Read More</div>
+					<div className="right-btn">Book now</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default CardItem;
