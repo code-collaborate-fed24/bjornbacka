855cafce7f6453b35a10d3fe5852f46adb2cbe3e
elinanomade <elinanomad@gmail.com>
2023-03-13 15:07:43 +0100

    add testimonials component with dropdown

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,20 +4,23 @@ import Header from "../../components/Header";
 import Kurser from "../../components/Kurser";
 import Partners from "../../components/Partners";
 import PartnerLogos from "../../components/partnersLogos";
+import Testimonial from "../../components/Testimonials";
 
 import "./home.css";
 function index() {
-	return (
-		<>
-			<div className="homepage-container">
-			  <Header />
-      <Partners />
-      <Kurser />
-      <PartnerLogos />
-      <Footer />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="homepage-container">
+        <Header />
+        <Partners />
+        <Kurser />
+        <PartnerLogos />
+        <Testimonial />
+
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default index;
