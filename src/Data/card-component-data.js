8af6be421749541ea3b710609dc5e6775eab3490
Gustav <gustav@kramnas.se>
2023-03-14 12:42:33 +0100

    fixed margin on buttons & changed text in data.js

--- src/Data/card-component-data.js
+++ src/Data/card-component-data.js
@@ -5,25 +5,25 @@ import Pic3 from "../Assets/CardComponentImages/pic3.JPG";
 const CardData = [
   {
     id: 1,
-    title: "BJÖRNBACKA ORIGINAL",
+    title: "Företag och konferanser",
     description:
-      "I det nybyggda centret finns den 210 kvm stora öppna kurslokalen med en imponerande utsikt över åkrar och hästhagar, den rymliga salongen på nedervåningen, 90 kvm takterrass för avslappnade pauser i solen eller kvällar med fantastiska solnedgångar",
+      "I det nybyggda centret finns den 210 kvm stora öppna kurslokalen med en imponerande utsikt över åkrar och hästhagar, den rymliga salongen på nedervåningen, 90 kvm takterrass för avslappnade pauser i solen eller kvällar med fantastiska solnedgångar.",
     // "Sovrum, antal bäddar och andra utrymmen.I Björnbacka Original finns 7 unika sovrum med minst en säng i varje (räkna med att dela rum). Är det en person per säng blir det 17 platser. Av de 17 sängarna är 5 dubbelsängar.",
     image: Pic1,
   },
   {
     id: 2,
-    title: "BJÖRNBACKA NYA CENTER",
+    title: "Host your own retreats",
     description:
-      "I det nybyggda centret finns den 210 kvm stora öppna kurslokalen med en imponerande utsikt över åkrar och hästhagar, den rymliga salongen på nedervåningen, 90 kvm takterrass för avslappnade pauser i solen eller kvällar med fantastiska solnedgångar",
+      "I det nybyggda centret finns den 210 kvm stora öppna kurslokalen med en imponerande utsikt över åkrar och hästhagar, den rymliga salongen på nedervåningen, 90 kvm takterrass för avslappnade pauser i solen eller kvällar med fantastiska solnedgångar.",
     image: Pic3,
   },
   {
     id: 3,
-    title: "BJÖRNBACKA EXLUSIVE",
+    title: "The leap & Deep leap",
     description:
       // "Ni har hela Björnbacka till ert förfogande; både Original och Nya Centret, samtliga trädgårdar och fristående byggnader samt parkering. Sammanlagt 27 sängplatser.",
-      "I det nybyggda centret finns den 210 kvm stora öppna kurslokalen med en imponerande utsikt över åkrar och hästhagar, den rymliga salongen på nedervåningen, 90 kvm takterrass för avslappnade pauser i solen eller kvällar med fantastiska solnedgångar",
+      "I det nybyggda centret finns den 210 kvm stora öppna kurslokalen med en imponerande utsikt över åkrar och hästhagar, den rymliga salongen på nedervåningen, 90 kvm takterrass för avslappnade pauser i solen eller kvällar med fantastiska solnedgångar.",
     image: Pic2,
   },
 ];
