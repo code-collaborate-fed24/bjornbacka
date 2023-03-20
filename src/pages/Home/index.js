7916e592a21b30685f5d79fa490e95f28364047a
amin <amin.haghi@hyperisland.se>
2023-03-20 13:27:45 +0100

    add partners page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Footer from "../../components/Footer";
-import Header from "../../components/Header";
 import Kurser from "../../components/Kurser";
 import Navbar from "../../components/Nav/Navbar";
 import Partners from "../../components/Partners";
@@ -8,7 +7,6 @@ import CardComponents from "../../components/CardComponents/cardcomponent";
 import PartnerLogos from "../../components/partnersLogos";
 import Testimonial from "../../components/Testimonials";
 import ImageCarousel from "../../components/ImageCarousel";
-import Events from "../../components/Events";
 import "./home.css";
 
 import Map from "../../components/Map";
@@ -20,13 +18,11 @@ function index() {
 			<div className="homepage-container">
 				<Navbar />
 				<CardComponents />
-				<Events />
+				<Hero />
 				<Testimonial />
 				<Partners />
 				<Kurser />
 				<PartnerLogos />
-				<Hero />
-				<Testimonial />
 				<ImageCarousel />
 				<Map />
 				<Footer />
--- src/pages/partners/index.js
+++ src/pages/partners/index.js
@@ -1,14 +1,43 @@
 import React from "react";
 import Footer from "../../components/Footer";
-import Header from "../../components/Header";
-import Partners from "../../components/Partners";
 import Navbar from "../../components/Nav/Navbar";
-
+import "./partners.css";
+import { datas } from "../../Data/data";
 function index() {
+	console.log(datas);
 	return (
 		<>
 			<Navbar />
-			<h1>partners page</h1>
+
+			<div className="partners-page">
+				<div className="first-content">
+					<p>
+						Vi samarbetar med några väl utvalda samarbetspartners vilka vi anser
+						har stort inflytande på vårt samhälles positiva utveckling. Våra
+						partners är med och samskapar Björnbackas riktlinjer med
+						meningsfullt innehåll till våra kunders företagskonferenser eller
+						andra events. Du som kund är välkommen att vända dig till oss med en
+						förfrågan där vi i samråd tar fram ett passande innehåll som är
+						förankrade i era behov.
+					</p>
+				</div>
+				<div className="user-container">
+					{datas.map((user) => {
+						return (
+							<div key={user.id} class="user-card">
+								<div className="user-card__left">
+									<img alt={user.id} src={user.img} />
+									<p>{user.name}</p>
+								</div>
+								<div className="user-card__right">
+									<p>{user.content}</p>
+								</div>
+							</div>
+						);
+					})}
+				</div>
+				<Footer />
+			</div>
 		</>
 	);
 }
