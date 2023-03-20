9bdefa50b5217a11305869573ca5eefea229d278
Gustav <gustav@kramnas.se>
2023-03-20 13:32:15 +0100

    margin in footer and map. Also text in testimonial

--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -8,15 +8,17 @@ const Testimonial = () => {
       <div className="One">
         <h2>Testimonials</h2>
         <p>
-          The Leap har gett mig möjligheten att kunna vara sårbar, öppna upp och
-          göra genuina skiften inombords som har stor betydelse för mitt liv.
-          Gett mig en möjlighet att se på händelser, upplevelser i mitt liv som
-          varit jobbiga, smärtsamma och skrämmande men också få möjlighet att
-          uttrycka och växa i min kraft, lycka och glädje vilket är det jag
+          "The Leap har gett mig möjligheten att kunna vara sårbar, öppna upp
+          och göra genuina skiften inombords som har stor betydelse för mitt
+          liv. Gett mig en möjlighet att se på händelser, upplevelser i mitt liv
+          som varit jobbiga, smärtsamma och skrämmande men också få möjlighet
+          att uttrycka och växa i min kraft, lycka och glädje vilket är det jag
           längtar efter. Upplevelserna och insikterna har inneburit stora
-          skillnader generellt och även i min yrkesroll.
+          skillnader generellt och även i min yrkesroll."
         </p>
-        <p>Gustav Widström - Egen företagare</p>
+        <p>
+          <strong>Gustav Widström - Egen företagare</strong>
+        </p>
       </div>
       <div className="Two">
         <img src="./images/leadershipsimage.png" alt="leadershipsimage" />
